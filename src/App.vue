50058cc2bd9342b8228d644b608c8fd6c4edf711
Joshua Mullins <joshua.stephen.mullins@gmail.com>
2026-03-01 16:16:52 -0600

    Completed video 9

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,90 @@
 <template>
-  <div>Hello, Vue!</div>
+  <div class="card">
+    <h2>Message: {{ message }}</h2>
+    <h2>Number: {{ number }}</h2>
+    <button @click="changeMessageToUpperCase">
+      Change message to upper case
+    </button>
+    <button @click="incrementNumber">Increment number</button>
+  </div>
+
+
+
+  <div class="card">
+    <h2>Name: {{ wizard.name }}</h2>
+    <h2>Wand: {{ wizard.wand }}</h2>
+    <button @click="changeNameToUpperCase">Change Name to Upper Case</button>
+    <button @click="changeWandCore">Change Wand Core</button>
+    <button @click="changeWizard">Change Wizard</button>
+    <button @click="wizard.wand.core = 'Unicorn hair'">Change Wand Core</button>
+  </div>
+
+
+
+  <div class="card">
+    <h2>Array: {{ wizards }}</h2>
+    <button @click="wizards.push('Draco')">Add a new wizard</button>
+  </div>
+  
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
+let message = ref('Hello, Reactivity!')
+let number = ref(1)
+
+function changeMessageToUpperCase() {
+  message.value = message.value.toUpperCase()
+  console.log(message.value)
+}
+function incrementNumber() {
+  number.value += 1
+  console.log(number.value)
+}
+
+let wizard = ref({
+  id: 1001,
+  name: 'Harry Potter',
+  house: 'Gryffindor',
+  age: 17,
+  wand: {
+    core: 'Phoenix feather',
+    wood: 'Holy'
+  }
+})
+
+function changeNameToUpperCase() {
+  wizard.value.name = wizard.value.name.toUpperCase()
+}
+
+function changeWandCore() {
+  wizard.value.wand.core = 'Unicorn Hair'
+}
+
+function changeWizard() {
+  wizard.value = {
+    id: 1002,
+    name: 'Hermione Granger',
+    house: 'Gryffindor',
+    age: 17,
+    wand: {
+      core: 'Dragon Heartstring',
+      wood: 'Vine'
+    }
+  }
+}
+
+let wizards = ref(['Harry', 'Hermione', 'Ron'])
+
 
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+.card {
+  background-color: purple;
+  color: white;
+  padding: 20px 10px;
+  margin-bottom: 10px;
+}
 </style>
